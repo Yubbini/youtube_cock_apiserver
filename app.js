cc89b4f66e2f1d742cb7c94d0d7f142966d03159
yubin <jjas8548@gmail.com>
2022-02-18 23:31:33 +0900

    connect mongoDB atlas

--- app.js
+++ app.js
@@ -4,20 +4,12 @@ const port = process.env.PORT || 80;
 const mongoose = require('mongoose');
 
 
-mongoose.connect(
-    process.env.MONGODB_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      dbName: "userInfo"
-    })
+mongoose.connect(process.env.MONGODB_URI)
     .then(()=>{
         console.log("Connected to MongoDB");
     })
     .catch((err)=>{
-        console.log("DB is error");
+        console.log(err);
     });
 
 app.get('/', (req, res) => res.send('Hello World!'))
