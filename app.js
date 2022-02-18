13e8899444b6c5c612ce03589e7a369977966c61
yubin <jjas8548@gmail.com>
2022-02-18 23:12:53 +0900

    Add mongoDB atlas

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 80;
+const mongoose = require('mongoose');
+
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      dbName: "userInfo"
+    })
+    .then(()=>{
+        console.log("Connected to MongoDB");
+    })
+    .catch((err)=>{
+        console.log("DB is error");
+    });
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
