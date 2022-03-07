8e03c83c68494539a276df07b7b5e24ee81acbac
HunJeong <junjw123@gmail.com>
2022-03-08 00:38:06 +0900

    Add test framework

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 'use strict'
 
-require('dotenv').config()
-
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+module.exports = app
