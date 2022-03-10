530922c0c73025795f280edd0b4ab1f984f541fc
HunJeong <junjw123@gmail.com>
2022-03-10 23:56:58 +0900

    Add swagger for documentation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,35 @@
 'use strict'
 
 const express = require('express')
+const swagger_ui = require('swagger-ui-express')
 const app = express()
 
+const swagger_on = process.env.SWAGGER_ON || 'true'
+
+if (swagger_on === 'true') {
+    app.use('/swagger_docs', swagger_ui.serve, swagger_ui.setup(require('./swagger_docs')))
+}
+
+/**
+ * @swagger
+ * tags:
+ *   name: HelloWorld
+ *   description: Health Checker
+ */
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Health Checker
+ *     tags: [HelloWorld]
+ *     produces:
+ *     - "application/text"
+ *     responses:
+ *       "200":
+ *         description: "successful operation"
+ *
+ */
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
---
+++ src/swagger_docs.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const host = process.env.HOST || 'localhost'
+const port = process.env.PORT || 8080
+
+const swaggerDefinition = {
+    info : { // 정보 작성
+        title : 'youtube_cock',
+        version : '1.0.0',
+        description : 'youtube_cock_docs'
+    },
+    host : `${host}:${port}`,
+    basePath : '/'
+};
+
+const options = {
+    swaggerDefinition: swaggerDefinition,
+    apis: [
+        `${__dirname}/routes/*.js`,
+        `${__dirname}/app.js`
+    ]
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
