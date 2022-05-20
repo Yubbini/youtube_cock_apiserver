4a5d475afb932de74f1fd1e26cdae4800507eb7e
임유빈 <jjas8548@gmail.com>
2022-05-20 16:09:58 +0900

    replace server.js

--- src/server.js
+++ src/server.js
@@ -5,19 +5,19 @@ require('dotenv').config()
 const app = require('./app')
 const port = process.env.PORT || 8080
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env['URL'], {useUnifiedTopology : true, useNewUrlParser: true});
+mongoose.connect(process.env['URL'], { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 
-db.on("error",()=>{
+db.on("error", () => {
     console.log("> error occurred from the database");
 });
 
-db.once("open", ()=>{
+db.once("open", () => {
     console.log("> successfully opened the database");
 });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
